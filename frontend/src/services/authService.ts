392ce5065754e04e8a0e32a93f6d6da6af92eb2b
SkyBlack-S1 <dinhthuan836@gmail.com>
2026-01-12 23:45:37 +0700

    Set up Zustand

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -8,6 +8,12 @@ export const authService = {
     firstName: string,
     lastName: string
   ) => {
-    const res = await api.post("/auth/signup");
+    const res = await api.post(
+      "/auth/signup",
+      { username, password, email, firstName, lastName },
+      { withCredentials: true }
+    );
+
+    return res.data;
   },
 };
--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -1,14 +1,20 @@
 import { create } from "zustand";
 import { toast } from "sonner";
-export const useAuthStore = create((set, get) => ({
+import { authService } from "@/services/authService";
+import type { AuthState } from "@/types/store";
+
+export const useAuthStore = create<AuthState>((set, get) => ({
   accessToken: null,
   user: null,
   loading: false, // theo dõi trạng thái khi gọi api
 
-  signUp: async () => {
+  signUp: async (username, password, email, firstName, lastName) => {
     try {
       set({ loading: true });
       // gọi api
+
+      await authService.signUp(username, password, email, firstName, lastName);
+
       toast.success(
         "Đăng ký thành công! Bạn sẽ được chuyển sang trang đăng nhập."
       );
